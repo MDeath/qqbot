23b4763211c20ad019a6916ae2ddec956e468e3a
MDeath <1064393873@qq.com>
2021-09-20 18:23:49 +0800

    新增凌晨4点定时任务：p站日榜

---
+++ qqbot/plugins/pixiv.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import re
+import time
+
+from plugins.admin import admin_ID
+from pixivpy3 import *
+import __soup as soup
+from qqbotcls import QQBotSched
+
+USERNAME = None
+PASSWORD = None
+REFRESH_TOKEN = None
+
+@QQBotSched(year=None, month=None, day=None, week=None, day_of_week=None, hour=4, minute=None, second=None, start_date=None, end_date=None, timezone=None)
+def day_ranking(bot):
+    if not hasattr(bot, 'pixiv'):
+        onPlug(bot)
+    api = bot.pixiv
+    _n = '\n'
+    n = 10
+    result = api.illust_ranking()
+    while n > 0:
+        for i in result.illusts:
+            if n < 1:break
+            if i.type != 'illust':continue
+            Plain = f'标题:{i.title} Pid:{i.id}{_n}作者:{i.user.name} Uid:{i.user.id}{_n}标签:'
+            for tag in i.tags:Plain += f'{_n}{tag.name}:{tag.translated_name}'
+            message = [soup.Plain(Plain)]
+            if i.page_count > 1:
+                for page in i.meta_pages:
+                    message.append(soup.Image(url=page.image_urls.original.replace('pximg.net','pixiv.cat')))
+            else:
+                message.append(soup.Image(url=i.meta_single_page.original_image_url.replace('pximg.net','pixiv.cat')))
+            for g in bot.Group:
+                bot.SendMessage('Group', g.id, message)
+            time.sleep(1)
+            n -= 1
+        if n > 0:
+            result = api.illust_ranking(**api.parse_qs(result.next_url))
+
+def onPlug(bot):
+    if not hasattr(bot, 'pixiv'):
+        api = ByPassSniApi()
+        api.require_appapi_hosts(hostname="public-api.secure.pixiv.net")
+        api.set_accept_language('zh-cn')
+        if REFRESH_TOKEN or (USERNAME and PASSWORD):
+            api.auth(USERNAME, PASSWORD, REFRESH_TOKEN)
+            setattr(bot,'pixiv',api)
+        else:
+            bot.Unplug(__file__)
+
+def onUnplug(bot):
+    if hasattr(bot, 'pixiv'):
+        delattr(bot, 'pixiv')
+
+def onInterval(bot):
+    if hasattr(bot, 'pixiv'):
+        onPlug(bot)
