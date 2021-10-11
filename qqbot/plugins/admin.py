25880109d348a0e6dc669a650b75bb6ddef634d1
M_Die <1064393873@qq.com>
2021-10-11 15:19:14 +0800

    改回对重启关机的操作

--- qqbot/plugins/admin.py
+++ qqbot/plugins/admin.py
@@ -136,12 +136,12 @@ def onQQMessage(bot, Type, Sender, Source, Message):
     if admin_ID(bot, Sender.id, True):
         if '重启' == Plain:
             bot.SendMessage(Type, target, [soup.Plain('bot正在重启')])
-            bot.Restart
+            Put(bot.Restart)
             return
 
         elif '关机' == Plain:
             bot.SendMessage(Type, target, [soup.Plain('bot以关闭')])
-            bot.Stop
+            Put(bot.Stop)
 
         elif '休眠' == Plain:
             for p in bot.Plugins():
