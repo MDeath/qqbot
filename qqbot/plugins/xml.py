36a8ce5e5141a82c36ec7be1e5d3d80077e04862
M_Die <1064393873@qq.com>
2022-09-21 17:25:57 +0800

    恢复v2.5.2

--- qqbot/plugins/xml.py
+++ qqbot/plugins/xml.py
@@ -30,7 +30,7 @@ def onQQMessage(bot, Type, Sender, Source, Message):
     for msg in Message:
         if msg.type == 'Quote':
             quote = msg.id
-            Quote = bot.MessageFromId(target, quote)
+            Quote = bot.MessageFromId(quote)
             break
     for msg in Message:
         if msg.type == 'Plain' and 'xml' in msg.text:
