243e96dd9f5a5fc231ea48aa8a84a567cfbb9a7f
M_Die <1064393873@qq.com>
2022-11-24 03:35:40 +0800

    整合代码

--- qqbot/plugins/qr.py
+++ qqbot/plugins/qr.py
@@ -1,18 +1,43 @@
 # -*- coding: utf-8 -*-
-import os, requests
+import os, requests, time
 from MyQR import myqr
 
 import soup
 
-def imgurltoqr(url,**kwargs):
-    tempdir = os.path.join(os.getcwd(),'temp')
+def basename(s:str):
+    s = os.path.basename(s)
+    for c in ['\\','/',':','*','?','"','<','>','|']:
+        s = s.replace(c,'_')
+    return s
+
+def imgurl2qr(
+        word=None,
+        picture:str=None,
+        **kwargs
+    ) -> soup.Image:
+    '''
+    picture:可以是图片位置,或是图片url
+    word:文本内容,为空时可以采用picture
+    '''
+    if not (word or picture):raise Exception('wrod or picture 不可同时为空')
+    
+    tempdir = os.path.join(os.getcwd(),f'temp/qr/{time.strftime("%Y-%m-%d",time.localtime())}')
     if not os.path.exists(tempdir):
         os.makedirs(tempdir)
-    picture = os.path.join(tempdir, os.path.basename(url))
-    with open(picture, "wb") as out_file:
-        out_file.write(requests.get(url,**kwargs).content)
+
+    if picture and picture.startswith('http'):
+        resp = requests.get(picture,**kwargs).content
+        picture = os.path.join(tempdir, basename(picture))
+        if picture[-4:] not in ('.jpg','.png','.bmp','.gif'):
+            if resp[6:10] in (b'JFIF', b'Exif'):picture+='.jpg'
+            elif resp.startswith(b'\211PNG\r\n\032\n'):picture+='.png'
+            elif resp[:6] in (b'GIF87a', b'GIF89a'):picture+='.gif'
+            elif resp.startswith(b'BM'):picture+='.bmp'
+            else:picture+='.png'
+        with open(picture, "wb") as out_file:out_file.write(resp)
+
     version, level, qr_name = myqr.run(
-        url,
+        word or picture,
         version = 1,
         level = 'H',
         picture = picture,
@@ -54,29 +79,12 @@ def onQQMessage(bot, Type, Sender, Source, Message):
     if not Plain.lower().strip().startswith('qr'):return
     words = Plain.strip()[2:]
 
-    tempdir = os.path.join(os.getcwd(),'temp')
-    if not os.path.exists(tempdir):
-        os.makedirs(tempdir)
     if Image:
-        if not words:words = Image[0].url
-        picture = os.path.join(tempdir, Image[0].imageId)
-        with open(picture, "wb") as out_file:
-            out_file.write(requests.get(Image[0].url).content)
-    else:picture = None
+        imgurl = Image[0].url
+        if not words:words = imgurl
+    else:imgurl = None
 
     if not words:
         bot.SendMessage(Type, target, soup.Plain('没有包含文本，或者关联图片，请尝试直接和图片一起发送'), id=Source.id)
         return
-    
-    version, level, qr_name = myqr.run(
-        words,
-        version = 1,
-        level = 'H',
-        picture = picture,
-        colorized = True,
-        contrast = 1.0,
-        brightness = 1.0,
-        save_name = None,
-        save_dir = tempdir
-    )
-    bot.SendMessage(Type, target, soup.Image(path=qr_name), id=Source.id)
+    bot.SendMessage(Type, target, imgurl2qr(words, imgurl), id=Source.id)
